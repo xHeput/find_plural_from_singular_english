6a4b46b574cab49d3b38eb9c86f6a5547f5663fa
xHeput <133649025+xHeput@users.noreply.github.com>
2023-05-15 13:25:23 +0200

    Add files via upload

---
+++ sigular_into_plural.py
@@ -0,0 +1,21 @@
+  # program will take singular form of word and find it's plural form
+
+import numpy
+from pattern.en import pluralize
+
+  # imports file where your singular words are
+file_name = 'English_words_singular.txt'
+import_file = numpy.loadtxt(file_name, delimiter=",", dtype=str)
+  # prints importer words
+print("Twoje słowa w l. pojedynczej",import_file)
+
+
+  # loop which uses pluralize() to find plural form for all words
+plural_file = []
+for i in range(len(import_file)):
+    plural_file.append(pluralize(import_file[i]))
+  # prints table with prulal words
+print("Twoje słowa w l. mnogiej",plural_file)
+
+numpy.savetxt('English_words_plural.txt', plural_file, delimiter=',', fmt='%s')
+
